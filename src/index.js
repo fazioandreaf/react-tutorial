785069a892dac54fb34ba11c27f02d9e76c5b0df
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-08-24 12:36:34 +0200

    utilizzo di funzione di componenti

--- src/index.js
+++ src/index.js
@@ -17,32 +17,64 @@ import './index.css';
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
-// singolo quadratino
-class Square extends React.Component {
-  constructor(props) {
-    // Nelle classi JavaScript, devi sempre chiamare super quando definisci il costruttore di una sottoclasse (classe derivata). Tutte le classi componente React che hanno un constructor devono sempre richiamare super(props) come prima istruzione nel costruttore.
-    super(props);
-    this.state={
-      value:null,
-    }
+// // singolo quadratino
+// class Square extends React.Component {
+//   constructor(props) {
+//     // Nelle classi JavaScript, devi sempre chiamare super quando definisci il costruttore di una sottoclasse (classe derivata). Tutte le classi componente React che hanno un constructor devono sempre richiamare super(props) come prima istruzione nel costruttore.
+//     super(props);
+//     this.state={
+//       // value:null,
+//     }
 
-  }
-  render() {
-    return (
-      // in questo modo arriva come props un oggetto
-      // <button className="square" onClick={()=>{console.log(this.props)}}>
-      // Quando chiamiamo setState in un componente, React aggiorna automaticamente anche i componenti figli al suo interno.
-      <button className="square" onClick={()=>{this.setState({value:'X'})}}>
-        {this.state.value}
-      </button>
-    );
-  }
+//   }
+//   render() {
+//     return (
+//       // in questo modo arriva come props un oggetto
+//       // <button className="square" onClick={()=>{console.log(this.props)}}>
+//       // Quando chiamiamo setState in un componente, React aggiorna automaticamente anche i componenti figli al suo interno.
+//       // <button className="square" onClick={()=>{this.setState({value:'X'})}}>
+//       <button className="square" onClick={()=>{this.props.functionProp()}}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+//  componenti funzione sono un modo più semplice di scrivere componenti che hanno il solo metodo render e non mantengono il proprio stato interno
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.functionProp}>
+      {props.value}
+    </button>
+  );
 }
+
 // faccio il campo
 class Board extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+  handleClick(i){
+    // console.log(i);
+    // console.log(this.state.squares);
+    // why slice, chiamiamo .slice() per creare una copia dell’array squares invece di modificare l’array esistente.
+    const squares = this.state.squares.slice();
+    // console.log(squares);
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   //inizio una funzione che mi crea un quadratino
   renderSquare(i) {
-    return <Square value={i} />;
+    return(
+     <Square 
+        value={this.state.squares[i]} 
+        functionProp={() => this.handleClick(i)} 
+      />
+      );
   }
 
   render() {
